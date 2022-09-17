47b0c13a99bcb81afde60888c5b2d736af25f2a9
seolha <kynwww@naver.com>
2022-09-17 16:31:58 +0900

    ✨ modal btn act

--- Basic Resouce/js/10000hours.js
+++ Basic Resouce/js/10000hours.js
@@ -1,1 +1,18 @@
 // js file
+
+const goBtn = document.querySelector(".go");
+const closeBtn = document.querySelector(".close");
+const modal = document.querySelector("#modal");
+
+const HIDDEN_NAME = "hidden";
+
+goBtn.addEventListener("click", onGoBtnClick);
+closeBtn.addEventListener("click", onCloseBtnClick);
+
+function onGoBtnClick(event) {
+  modal.classList.remove(HIDDEN_NAME);
+}
+
+function onCloseBtnClick(event) {
+  modal.classList.add(HIDDEN_NAME);
+}
